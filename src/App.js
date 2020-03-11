d07ddc8e2e56dbe92570a62a84bf3f20613d50cf
Claire Powers <clairepowers@claires-mbp-3.wkstn.qmul.ac.uk>
2020-03-11 10:13:18 +0000

    got api working

--- src/App.js
+++ src/App.js
@@ -1,53 +1,40 @@
 import React, { Component } from 'react';
 import './App.css';
+import WeekContainer from './WeekContainer';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      items: []
-    };
+  state = {
+    isLoading: true,
+    users: [],
+    error: null
+  };
+
+  fetchUsers() {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&APPID=b0ea3a08c599d478b89e1c280d32dedc`)
+      .then(response => response.json())
+      .then(data =>
+        this.setState({
+          users: data,
+          isLoading: false,
+        })
+      )
+      .catch(error => this.setState({ error, isLoading: false }));
   }
 
   componentDidMount() {
-    fetch("http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&APPID=b0ea3a08c599d478b89e1c280d32dedc")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            items: result.items
-          });
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error: true
-          });
-        }
-      )
+    this.fetchUsers();
   }
-
   render() {
-    const { error, isLoaded, items } = this.state;
+    const { isLoading, users, error } = this.state;
     if (error) {
       return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
+    } else if (isLoading) {
       return <div>Loading...</div>;
     } else {
       return (
-        <ul>
-          {items.map(item => (
-            <li key={item.name}>
-              {item.name} {item.price}
-            </li>
-          ))}
-        </ul>
+        <div>
+              {users.name}
+        </div>
       );
     }
   }
