894d20d316118d6c11e3bc8370e620589003dc10
Claire Powers <clairepowers@claires-mbp-3.wkstn.qmul.ac.uk>
2020-03-11 11:16:09 +0000

    adding bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './App.css';
+import NavBar from './NavBar.js';
+
 
 class App extends Component {
   state = {
@@ -27,11 +32,27 @@ class App extends Component {
       return <div>Loading...</div>;
     } else {
       return (
-        <div>
-              <h1>{weatherdata.name}</h1>
-              Temperature: {weatherdata.main.temp} <br/>
-              Windspeed: {weatherdata.wind.speed}
+        <div className="App">
+        <header class="masthead mb-auto">
+          <NavBar />
+        </header>
+        <body class="text-center">
+          <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+
+            <h1>{weatherdata.name}</h1>
+            <p class="lead">
+            <div class = "degrees" >{weatherdata.main.temp}°</div><br/>
+            F C <br/>
+            images <br/>
+            Windspeed: {weatherdata.wind.speed} <br/>
+            You should wear ... <br/>
+            M T W Th F Sa Su
+            </p>
+          </div>
+        </body>
         </div>
+
+
       );
     }
   }
---
+++ src/NavBar.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class NavBar extends Component{
+  render(){
+    return(
+      <nav class="navbar navbar-dark">
+        <div class="container d-flex justify-content-between">
+          <a href="#" class="navbar-brand d-flex align-items-center"> <strong>Title</strong> </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        </div>
+      </nav>
+    )
+  }
+}
+
+export default NavBar;
